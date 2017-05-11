79059cc54c91f8cf8e30695d0b33fced15bf240d
Aleksander Bizjak <aleksander.bizjak@pearson.com>
2017-05-11 10:15:19 +0100

    Updated regex to accept negative top and bottom range values
    Added a test for negative top and bottom range values

--- query_builder/tests/end_to_end/test_ranges.py
+++ query_builder/tests/end_to_end/test_ranges.py
@@ -42,7 +42,22 @@ def test_range_top_end():
     response = get_es_query(url)
     assert response == revenue_range_template(None, 100)
 
-def test_range_negative_values():
+def test_range_negative_bottom_end():
+    url = "/v1/company_query_builder?revenue=-100-"
+    response = get_es_query(url)
+    assert response == revenue_range_template(-100, None)
+
+def test_range_negative_top_end():
+    url = "/v1/company_query_builder?revenue=--100"
+    response = get_es_query(url)
+    assert response == revenue_range_template(None, -100)
+
+def test_range_negative_to_positive_ends():
     url = "/v1/company_query_builder?revenue=-100-1000"
     response = get_es_query(url)
     assert response == revenue_range_template(-100, 1000)
+
+def test_range_negative_to_negative_ends():
+    url = "/v1/company_query_builder?revenue=-100--10"
+    response = get_es_query(url)
+    assert response == revenue_range_template(-100, -10)
